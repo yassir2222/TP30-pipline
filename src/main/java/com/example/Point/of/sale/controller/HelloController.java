6320c16fd5fbad697513022ccb594ceff782d12f
yassirLambrass <yassir.lambrass@gmail.com>
2025-12-16 15:06:03 +0100

    test CI

--- src/main/java/com/example/Point/of/sale/controller/HelloController.java
+++ src/main/java/com/example/Point/of/sale/controller/HelloController.java
@@ -9,8 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/")
 public class HelloController {
     @GetMapping
-    public String hello(){
-        return "Hello from spring boot application :)";
+    public String hello() {
+        return "Hello from New Test :)";
     }
 
     @GetMapping("/user")
@@ -23,4 +23,5 @@ public class HelloController {
     public String getPresentation(){
         return "presentation";
     }
+
 }
